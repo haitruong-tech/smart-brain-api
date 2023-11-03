a652c43e22072348cdad989af21afb95e7b4b306
Truong Quy Hai <haitruong.tech@gmail.com>
2023-11-03 10:38:23 +0700

    Add Database

--- server.js
+++ server.js
@@ -2,85 +2,91 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
+
+const db = require("knex")({
+  client: "pg",
+  connection: {
+    host: "127.0.0.1",
+    port: 5432,
+    user: "quyhaitruong",
+    database: "smart-brain",
+    // connectionString: config.DATABASE_URL,
+    // password: config["DB_PASSWORD"],
+    // ssl: config["DB_SSL"] ? { rejectUnauthorized: false } : false,
+  },
+});
+
 const { calculateFaceBoundingBox } = require("./services/clarifai");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "John",
-      email: "john@gmail.com",
-      password: "cookies",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "Sally",
-      email: "sally@gmail.com",
-      password: "bananas",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-};
-
-app.get("/", (req, res) => {
-  res.json(database.users);
+app.get("/", async (req, res) => {
+  res.json(await db.select("*").from("users"));
 });
 
-app.post("/signin", (req, res) => {
+app.post("/signin", async (req, res) => {
   const { email, password } = req.body;
-  if (
-    email === database.users[0].email &&
-    password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
+  const user = await db.select("*").from("users").where("email", email);
+  if (user[0] == null) {
+    return res.status(400).json("Error Logging In");
+  }
+  const hash = await db.select("hash").from("login").where("email", email);
+  const validPassword = await bcrypt.compare(password, hash[0]?.hash ?? "");
+  if (validPassword) {
+    res.json(user[0]);
   } else {
-    res.status(400).json("Error Logging in");
+    res.status(400).json("Error Logging In");
   }
 });
 
-app.post("/register", (req, res) => {
-  const { name, email, password } = req.body;
-  const newUser = {
-    id: "125",
-    name,
-    email,
-    password,
-    entries: 0,
-    joined: new Date(),
-  };
-  database.users.push(newUser);
-  res.json({
-    id: newUser.id,
-    name,
-    email,
-    entries: newUser.entries,
-    joined: newUser.joined,
+app.post("/register", async (req, res) => {
+  db.transaction(async (trx) => {
+    try {
+      const { name, email, password } = req.body;
+      await trx
+        .insert({
+          hash: await bcrypt.hash(password, 10),
+          email,
+        })
+        .into("login");
+      const newUser = await trx
+        .returning("*")
+        .insert({
+          name,
+          email,
+        })
+        .into("users");
+      res.json(newUser[0]);
+    } catch (error) {
+      console.error(error);
+      res.status(400).json("Unable to register");
+    }
   });
 });
 
-app.get("/profile/:id", (req, res) => {
+app.get("/profile/:id", async (req, res) => {
   const { id } = req.params;
-  const user = database.users.find((user) => user.id === id);
-  if (user) return res.json(user);
+  const user = await db.select("*").from("users").where("id", id);
+  if (user[0]) return res.json(user[0]);
   res.status(404).json("No such user");
 });
 
 app.post("/image/face-detect", async (req, res) => {
-  const { userID, imageURL } = req.body;
-  const user = database.users.find((user) => user.id === userID);
-  const boundingBoxes = await calculateFaceBoundingBox(imageURL);
-  if (user) {
-    user.entries++;
-    return res.json({ entries: user.entries, boundingBoxes });
+  try {
+    const { userID, imageURL } = req.body;
+    const result = await db
+      .where("id", userID)
+      .increment({ entries: 1 })
+      .into("users")
+      .returning("entries");
+    const boundingBoxes = await calculateFaceBoundingBox(imageURL);
+    res.json({ entries: result[0].entries, boundingBoxes });
+  } catch (error) {
+    res.status(500).json("Something went wrong");
   }
-  res.status(404).json("No such user");
 });
 
 app.listen(3000, () => {
--- services/clarifai.js
+++ services/clarifai.js
@@ -1,16 +1,5 @@
 const calculateFaceBoundingBox = async (image_url) => {
-  ///////////////////////////////////////////////////////////////////////////////////////////////////
-  // In this section, we set the user authentication, user and app ID, model details, and the URL
-  // of the image we want as an input. Change these strings to run your own example.
-  //////////////////////////////////////////////////////////////////////////////////////////////////
-
-  // TODO: migrate to server so we can protect the secret keys
-  // Your PAT (Personal Access Token) can be found in the portal under Authentification
   const { PAT, USER_ID, APP_ID, MODEL_ID } = process.env;
-
-  ///////////////////////////////////////////////////////////////////////////////////
-  // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-  ///////////////////////////////////////////////////////////////////////////////////
 
   const raw = JSON.stringify({
     user_app_id: {
@@ -36,10 +25,6 @@ const calculateFaceBoundingBox = async (image_url) => {
     },
     body: raw,
   };
-
-  // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-  // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-  // this will default to the latest version_id
 
   try {
     const response = await fetch(
